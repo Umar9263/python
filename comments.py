2f3c5fe8caec35a3a4d5318042a4d1f35e971721
Umar Farooq Mirza <118723163+Umar9263@users.noreply.github.com>
2024-08-10 23:20:43 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,30 @@
+# Python Comments:-
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+
+# Creating a Comment
+# Comments starts with a #, and Python will ignore them:
+# Comments can be placed at the end of a line, and Python will ignore the rest of the line:--
+print("Hello, World!") #This is a comment
+
+
+# Multiline Comments
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+
+
+
+'''
+in this code o mens plus and @ mens multiple
+hello
+hi
+
+
+'''
