a467ea5460d175037c718029e84665d45b58a8e8
Umar Farooq Mirza <118723163+Umar9263@users.noreply.github.com>
2024-08-29 21:48:42 +0530

    basic.py

--- basic.py
+++ basic.py
@@ -27,14 +27,14 @@
         # Python runs on an interpreter (read code line by line)system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
         # Python can be treated in a procedural way(break down program into smaller , use variable ,loop and conditions), an object-oriented way or a functional way.
 
-# ///////////////////////////////
+# /////////////////////////
 
 # Python Syntax compared to other programming languages:
         # Python was designed for readability, and has some similarities to the English language with influence from mathematics.
         # Python uses new lines to complete a command, as opposed to other programming languages which often use semicolons or parentheses.
         # Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. Other programming languages often use curly-brackets for this purpose.
 
-# ////////////////////////////
+# /////////////////////
 
 # for example :-
 
